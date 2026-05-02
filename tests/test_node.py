6df349addf7368e369b0147e5e3ddcfa13d1c00e
Ian Patterson <cr31.dev@gmail.com>
2026-05-01 21:59:42 -0500

    rename some troublesome methods related to internal datastores and improve overall doc coverage of streamableresource.py

--- tests/test_node.py
+++ tests/test_node.py
@@ -15,10 +15,10 @@ def test_apihelper_url_generation():
     assert helper.get_api_root_url(socket=True) == "wss://localhost:8282/sensorhub/api"
 
 
-def test_node_password_round_trips_through_serialization():
+def test_node_password_round_trips_through_storage_dict():
     node = Node(protocol='http', address='localhost', port=8080,
                 username='user', password='pass')
-    serialized = node.serialize()
-    assert serialized['password'] == 'pass'
-    deserialized = Node.deserialize(serialized)
-    assert deserialized._api_helper.password == 'pass'
+    stored = node.to_storage_dict()
+    assert stored['password'] == 'pass'
+    rehydrated = Node.from_storage_dict(stored)
+    assert rehydrated._api_helper.password == 'pass'
